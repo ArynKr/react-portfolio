38b2c97004fd4d66abd6a9881113f3d50276bc62
Aryan Kumar <aryankumar1504@gmail.com>
2022-03-04 23:19:10 +0530

    Init: Portfolio section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const Portfolio = () => {
-	return <section id='portfolio'>Portfolio</section>;
+	return (
+		<section id='portfolio'>
+			<h5>My Recent Work</h5>
+			<h2>Portfolio</h2>
+
+			<div className='container portfolio__container '>
+				<article className='portfolio__item portfolio__item-image'>
+					<a href='' className='btn'></a>
+					<a href='' className='btn'></a>
+				</article>
+			</div>
+		</section>
+	);
 };
 
 export default Portfolio;
