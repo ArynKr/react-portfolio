d0fdab263a4493d3fc99b221b993b2e69d01a54c
Aryan Kumar <aryankumar1504@gmail.com>
2022-03-04 22:49:36 +0530

    Added Services Section

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,146 @@
-import React from 'react';
+import { BiCheck } from 'react-icons/bi';
+
+import './services.css';
 
 const Services = () => {
-	return <section id='services'>Services</section>;
+	return (
+		<section id='services'>
+			<h5>What I Offer</h5>
+			<h2>Services</h2>
+
+			<div className='container services__container'>
+				<article className='service'>
+					<div className='service__head'>
+						<h3>Frontend Development</h3>
+					</div>
+					<ul className='service__list'>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+					</ul>
+				</article>
+
+				<article className='service'>
+					<div className='service__head'>
+						<h3>Backend Development</h3>
+					</div>
+					<ul className='service__list'>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+					</ul>
+				</article>
+
+				<article className='service'>
+					<div className='service__head'>
+						<h3>UI Designing</h3>
+					</div>
+					<ul className='service__list'>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+						<li>
+							<BiCheck className='service__list-icon' />
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing
+								elit.
+							</p>
+						</li>
+					</ul>
+				</article>
+			</div>
+		</section>
+	);
 };
 
 export default Services;
